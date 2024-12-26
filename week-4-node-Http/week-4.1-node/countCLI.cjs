192de822ac67e6d5c0ac394af6ddd6a04fd8286b
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-12-26 23:53:23 +0500

    add the words count

--- week-4-node-Http/week-4.1-node/countCLI.cjs
+++ week-4-node-Http/week-4.1-node/countCLI.cjs
@@ -7,7 +7,7 @@ program
     .description("CLI to do file based tasks")
     .version('0.8.0');
 
-program.command("count")
+program.command("count-sentences")
     .description("Count the number of lines in a file ")
     .argument('<file>', "file to count")
     .action((file) => {
@@ -17,6 +17,20 @@ program.command("count")
             } else {
                 const lines = data.split('\n').length;
                 console.log(`There are ${lines} lines in ${file}`);
+            }
+        })
+    })
+
+program.command("count-words")
+    .description("Count the number of words in a file ")
+    .argument('<file>', "file to count words")
+    .action((file) => {
+        fs.readFile(file, "utf8", (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const words = data.split(' ').length;
+                console.log(`There are ${words} words in ${file}`);
             }
         })
     })
