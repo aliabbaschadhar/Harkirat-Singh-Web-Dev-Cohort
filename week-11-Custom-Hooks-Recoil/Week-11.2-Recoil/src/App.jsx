f0047a5c1262af8f4ac0cdb24dfaca59fa32287c
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-01-16 16:59:09 +0500

    Recoil atom and Selector

--- week-11-Custom-Hooks-Recoil/Week-11.2-Recoil/src/App.jsx
+++ week-11-Custom-Hooks-Recoil/Week-11.2-Recoil/src/App.jsx
@@ -39,9 +39,20 @@ function Counter() {
 }
 
 function IsEven() {
-  const even = useRecoilValue(evenSelector);
+  const even = useRecoilValue(evenSelector); // This is a derived state(Selector) that depends on countAtom atom and isEven will only re-render if the value of state in evenSelector changes like "even" to "odd".
+  // It does not re-render the component if the value is changed by 24 to 26 in countAtom.
+  // This is because evenSelector is a derived state.
+  // IsEven is subscribed only to evenSelector and not to countAtom
+
+  const count = useRecoilValue(countAtom); // Now if the value of state changes in countAtom the component will re-render.
+  // Bcz const count is subscribed to countAtom 
+  // As we know that component will re-render if the state in the atom changes.
+
 
   return <div>
     {even ? "Even" : "Odd"}
+    {/* {count % 2 == 0 ? "Even" : "Odd"}
+      Now as we know that count is not a derived state so it will re-render when the value of count changes in the countAtom.
+    */}
   </div>
 }
