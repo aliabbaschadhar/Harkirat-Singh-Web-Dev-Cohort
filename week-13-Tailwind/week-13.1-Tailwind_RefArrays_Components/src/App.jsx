11255bd1444de654a041081f66f087b0272257f4
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-01-20 22:38:08 +0500

    week-13.1

---
+++ week-13-Tailwind/week-13.1-Tailwind_RefArrays_Components/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+  return (
+    <div className="md:grid md:grid-cols-12">
+      <div className="bg-blue-400 md:grid-cols-5">This is first Div</div>
+      <div className="bg-red-400 md:grid-cols-5"> This is second Div</div>
+      <div className="bg-green-400 md:grid-cols-2">This is third Div</div>
+    </div>
+  )
+}
