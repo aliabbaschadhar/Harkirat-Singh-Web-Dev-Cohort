925af22e63c00ad0c2eca2e87217e6051c12f4d5
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-01-21 23:58:09 +0500

    week 13.2 tailwind project

---
+++ week-13-Tailwind/week-13.2-TailwindProject/src/App.jsx
@@ -0,0 +1,12 @@
+import Otp from './components/Otp'
+
+function App() {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ week-13-Tailwind/week-13.2-TailwindProject/src/components/Otp.jsx
@@ -0,0 +1,48 @@
+import e from "cors";
+import { useRef } from "react"
+
+
+export default function Otp({ number }) {
+    const refs = useRef([]);
+    return (
+        <div className="flex justify-center gap-3">
+            {Array.from({ length: number }).map((_, index) =>
+                <SubOpt
+                    key={index}
+                    refrence={(ele) => refs.current[index] = ele}
+                    onDone={() => {
+                        if (index >= number - 1) return;
+                        refs.current[index + 1].focus();
+                    }}
+                    onBack={() => {
+                        if (index <= 0) return;
+                        refs.current[index - 1].focus();
+                    }}
+                />
+            )}
+        </div>
+    )
+}
+
+function SubOpt({ refrence, onDone, onBack }) {
+
+    return (
+        <div>
+            <input
+                type="text"
+                className="border border-blue-200 w-10 h-10 rounded-md mt-6"
+                maxLength={1}
+                ref={refrence}
+                onChange={(e) => {
+                    !isNaN(e.target.value) && onDone();
+                }}
+                onKeyUp={(e) => {
+                    if (e.key === "Backspace") {
+                        e.target.value = "";
+                        onBack();
+                    }
+                }}
+            />
+        </div>
+    )
+}
