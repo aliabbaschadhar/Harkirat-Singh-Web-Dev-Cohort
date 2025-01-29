7789e2c912fc136d124672200b6af412cd7dcf3a
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-01-30 01:09:37 +0500

    brainly frontend

---
+++ W15-Brainly/W-15.2-Brainly-Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-3xl font-bold underline bg-amber-300'>This is tailwind</div>
+    </>
+  )
+}
+
+export default App
