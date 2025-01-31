d2464df7f4933b28b5678233ceada782e219be90
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-01 02:35:16 +0500

    commit websockets frontend

---
+++ W16-WebSockets/W-16.1-WebSockets/W-16.1-Websockets-FE/src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState();
+  const inputRef = useRef();
+
+  function sendMessage() {
+    if (!socket) {
+      return;
+    }
+    if (!inputRef.current) {
+      return;
+    }
+    const msg = inputRef.current.value;
+    //@ts-ignore
+    socket.send(msg);
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+
+    // To received message
+    ws.onmessage = (event) => {
+      alert(event.data)
+    }
+  }, [])
+
+  return (
+    <div>
+      <input
+        // @ts-ignore
+        ref={inputRef}
+        type="text"
+        className='bg-gray-300 text-black p-2 rounded-lg mr-2'
+      />
+      <button onClick={sendMessage}> Send</button>
+    </div>
+  )
+}
+
+export default App
