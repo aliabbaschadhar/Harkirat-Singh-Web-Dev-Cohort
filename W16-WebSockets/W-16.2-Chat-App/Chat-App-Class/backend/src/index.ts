f3880a95a913a4f849bd728f0682946c27e58af9
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-02 01:39:35 +0500

    Backend of chat app completed,Hardiii

--- W16-WebSockets/W-16.2-Chat-App/Chat-App-Class/backend/src/index.ts
+++ W16-WebSockets/W-16.2-Chat-App/Chat-App-Class/backend/src/index.ts
@@ -20,11 +20,45 @@ let allSockets1: User[] = []
 wss.on("connection", (socket) => {
 
     socket.on("message", (message) => {
+        // Here message will come in the form of string not an object it will come like:
+        //"{
+        //  'type':'join',
+        //   'payload':'{
+        //      'room':'12114312',
+        //    }'
+        // }"
 
+        // So we will convert it into in the form of an js object
+
+        const parsedMessage = JSON.parse(message as unknown as string);
+
+
+        if (parsedMessage.type === "join") {
+
+            console.log("User wants to join the room: " + parsedMessage.payload.roomId)
+
+            allSockets1.push({
+                socket,
+                room: parsedMessage.payload.roomId,
+            })
+        }
+
+        if (parsedMessage.type === 'chat') {
+            const currentUserRoom = allSockets1.find((x) => x.socket === socket)?.room; // If the message sender rooom exists in allSockets1 array then find the room of that user.
+
+            console.log("User wants to chat ");
+
+            allSockets1.forEach((arraySocket) => {
+                if (arraySocket.room === currentUserRoom) {
+                    arraySocket.socket.send(parsedMessage.payload.message); // We were sending the message to the socket which is listening to the message instead of the socket which is sending the message.
+                }
+            })
+
+        }
     })
 
     socket.on("disconnect", () => {
-        allSockets1 = allSockets1.filter(x => x != socket)
+        allSockets1 = allSockets1.filter(x => x.socket !== socket)
     })
 })
 
@@ -34,22 +68,22 @@ wss.on("connection", (socket) => {
 
 //****************************Simple Chat App ******************* */
 
-let allSockets: WebSocket[] = [];
+// let allSockets: WebSocket[] = [];
 
-wss.on("connection", (ws) => {
+// wss.on("connection", (ws) => {
 
-    allSockets.push(ws);
-    console.log("User connected");
+//     allSockets.push(ws);
+//     console.log("User connected");
 
-    // To receive message from the client and then send to other client connected to the same webserver.
-    ws.on("message", (msg) => {
-        console.log("Message received " + msg.toString());
-        allSockets.forEach((socket) => socket.send(msg.toString() + " : To all users "));
-        // ws.send(msg.toString() + ": sent from the server");
-    })
+//     // To receive message from the client and then send to other client connected to the same webserver.
+//     ws.on("message", (msg) => {
+//         console.log("Message received " + msg.toString());
+//         allSockets.forEach((socket) => socket.send(msg.toString() + " : To all users "));
+//         // ws.send(msg.toString() + ": sent from the server");
+//     })
 
-    // Handling dead sockets if any of socket is dead then the message is not sent to that socket by server.
-    ws.on("disconnect", () => {
-        allSockets = allSockets.filter((workingSocket) => workingSocket !== ws)
-    })
-})
+//     // Handling dead sockets if any of socket is dead then the message is not sent to that socket by server.
+//     ws.on("disconnect", () => {
+//         allSockets = allSockets.filter((workingSocket) => workingSocket !== ws)
+//     })
+// })
