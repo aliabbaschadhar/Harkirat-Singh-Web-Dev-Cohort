6244e9aaa618a56c5defb984477faddc1f931a89
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-03 08:12:02 +0500

    Transaction in DB

--- W17-PostGresDB/W17.2-PostGresDB/src/index.ts
+++ W17-PostGresDB/W17.2-PostGresDB/src/index.ts
@@ -15,12 +15,22 @@ const pgClient = new Client({
 pgClient.connect();
 
 app.post("signup", async (req, res) => {
-    const { username, password, email } = req.body;
+    const { username, password, email, city, country, street, pincode } = req.body;
 
     try {
-        const inserQuery = `INSERT INTO users (username,password,email) VALUES ($1,$2,$3)`;
+        const insertQuery = `INSERT INTO users (username,password,email) VALUES ($1,$2,$3) RETURNING id`;
+        const insertAddressQuery = `INSERT INTO addresses (city,country,street,pincode,user_id) VALUES ($1,$2,$3,$4,$5)`
 
-        const response = await pgClient.query(inserQuery, [username, password, email]);
+        // transaction begins here
+        await pgClient.query("BEGIN;")
+
+        const response = await pgClient.query(insertQuery, [username, password, email]);
+
+        const user_id = response.rows[0].id;
+
+        const addressInsertResponse = await pgClient.query(insertAddressQuery, [city, country, street, pincode, user_id])
+
+        await pgClient.query("COMMIT;") //End here
 
         res.json({
             msg: "You have signUp"
