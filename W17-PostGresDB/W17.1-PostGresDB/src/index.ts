ba46bdac84c76d846e0e75458dd30b6f133bf0fb
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-03 07:25:32 +0500

    SQL injection, parameterized variables

--- W17-PostGresDB/W17.1-PostGresDB/src/index.ts
+++ W17-PostGresDB/W17.1-PostGresDB/src/index.ts
@@ -18,7 +18,15 @@ app.post("signup", async (req, res) => {
     const { username, password, email } = req.body;
 
     try {
+        // SQL injection is a technique where an attacker injects malicious SQL code in a web application to access, modify or manipulate the database.
+        // In this case we are directly using the user input in our SQL query which can be harmfull.
+        // For example if an attacker passes a string like "SELECT * FROM users" in the username field, it will be injected in the query and the database will return all the rows.
+        // To prevent this we should use parameterized queries where the user input is passed as a parameter to the query.
+
+        //! const inserQuery = `INSERT INTO users (username,password,email) VALUES ('${username}','${password}','${email}')`;
+
         const inserQuery = `INSERT INTO users (username,password,email) VALUES ($1,$2,$3)`;
+        // Using parameterized queries prevents SQL injection by separating SQL code from user input. User inputs are passed as parameters, which the database treats as data only, not executable code.
 
         const response = await pgClient.query(inserQuery, [username, password, email]);
 
