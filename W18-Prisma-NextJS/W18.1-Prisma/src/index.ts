a5ac836d8183894269accbfec350a9735e8c3cbf
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-05 00:51:57 +0500

    Adding a user using prisma

--- W18-Prisma-NextJS/W18.1-Prisma/src/index.ts
+++ W18-Prisma-NextJS/W18.1-Prisma/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+// Creating user using prisma
+async function createUser() {
+    try {
+        const newUser = await client.user.create({
+            data: {
+                username: "john_doe",
+                password: "secure_pass",
+                age: 30,
+                city: "New York",
+            }
+        });
+        console.log("User created successfully:", newUser);
+    } catch (error) {
+        console.error("Error creating user:", error);
+    }
+}
+
+createUser();
+
