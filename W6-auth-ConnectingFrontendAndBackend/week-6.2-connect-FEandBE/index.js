d9b4564a2829e2306cb70f7b0f35b7a517f4bc90
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-03-30 22:50:31 +0500

    week 6.2 revision done

--- W6-auth-ConnectingFrontendAndBackend/week-6.2-connect-FEandBE/index.js
+++ W6-auth-ConnectingFrontendAndBackend/week-6.2-connect-FEandBE/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "ilovemyLOveBAby"
+const JWT_SECRET = "Bapastunning"
 
 const users = [];
 
