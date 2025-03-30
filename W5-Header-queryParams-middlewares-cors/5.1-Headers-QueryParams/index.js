9c6d9657b4d42f09380db233a7673fa8476e9a7a
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-03-30 05:59:04 +0500

    Added json.strigify(data,null,4) use of second parameter in this method example added.

--- W5-Header-queryParams-middlewares-cors/5.1-Headers-QueryParams/index.js
+++ W5-Header-queryParams-middlewares-cors/5.1-Headers-QueryParams/index.js
@@ -1,53 +1,76 @@
 const express = require("express");
 const app = express();
 
+// Example of use of second parameter of JSON.stringify() which is used to manipulate the JSON output
+const data = {
+    name: "John",
+    age: 30,
+    address: {
+        city: "New York",
+        state: "NY",
+        zip: 10001
+    }
+    ,
+    hobbies: ["reading", "traveling", "cooking"],
+    password: "secret",
+}
 
-app.get("/multiply",(req,res)=>{
-    const a =  parseInt(req.query.a);
-    const b =  parseInt(req.query.b);
+function replacer(key, value) {
+    if (key === "password") {
+        return "**********"
+    }
+    return value;
+}
+
+console.log(JSON.stringify(data, replacer, 4));
+
+app.get("/multiply", (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
     res.json({
-        ans : a*b
+        ans: a * b
     })
 })
 
 //Getting input from dynamic routes 
 // add/89/2
-app.get("/add/:a/:b",(req,res)=>{
+app.get("/add/:a/:b", (req, res) => {
     const a = parseInt(req.params.a);
     const b = parseInt(req.params.b);
     res.json({
-        ans:a+b
+        ans: a + b
     })
 })
 
 // add?a=89&b=2
-app.get("/add",(req,res)=>{
-    const a =  parseInt(req.query.a);
-    const b =  parseInt(req.query.b);
+app.get("/add", (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
     res.json({
-        ans : a+b
+        ans: a + b
     })
 })
 
 
-app.get("/subtract",(req,res)=>{
-    const a =  parseInt(req.query.a);
-    const b =  parseInt(req.query.b);
+app.get("/subtract", (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
     res.json({
-        ans : a-b
+        ans: a - b
     })
 })
 
 
 
-app.get("/divide",(req,res)=>{
-    const a =  parseInt(req.query.a);
-    const b =  parseInt(req.query.b);
+app.get("/divide", (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
     res.json({
-        ans : a/b
+        ans: a / b
     })
 })
 
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log("Running on port 3000 ...")
 });
