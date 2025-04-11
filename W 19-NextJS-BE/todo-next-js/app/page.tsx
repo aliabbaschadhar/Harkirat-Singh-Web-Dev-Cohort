63b9ca8e6d813f4ee71d03c448e5c9c6c2398daa
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-11 11:16:35 +0500

    Added the signup functionality on FE

---
+++ W 19-NextJS-BE/todo-next-js/app/page.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import Link from "next/link";
+// import { useRouter } from "next/navigation";
+
+export default function Home() {
+  // const router = useRouter()
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        Todo Application
+        {/* Bad way to do routing */}
+        {/* <button
+          onClick={() => {
+            router.push("/signin")
+          }}
+            To do that we have to make this componet client and by doing that we will the nextjs server optimization.
+            We also can don't prefetching here.
+        >SignIn</button> */}
+
+        {/* Doing routing from one page to another page */}
+        <br />
+        <Link className="text-md border-2 border-amber-700 m-2" href={"/signin"}>Sign-in Todo</Link>
+
+        <br />
+        <Link className="text-md border-2 border-amber-700 m-2" href={"/signup"}>Sign-up Todo</Link>
+
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
