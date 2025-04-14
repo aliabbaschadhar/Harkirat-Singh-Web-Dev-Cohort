5f6d2115e5d9eaedb1905a86492a999fe39a5ad7
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-15 01:21:34 +0500

    Signup endpoint done

--- W 21(offline)/Paytm-Basic/paytm-BE/src/routes/user.ts
+++ W 21(offline)/Paytm-Basic/paytm-BE/src/routes/user.ts
@@ -1,8 +1,53 @@
 import { Router } from "express";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { userModel } from "../db/schema";
 
 const userRouter = Router();
 
+userRouter.post("/signup", async (req, res) => {
+    const requiredBody = z.object({
+        username: z.string().trim().toLowerCase().min(3).max(30),
+        password: z.string().min(6),
+        firstName: z.string().trim().max(50),
+        lastName: z.string().trim().max(50),
+    })
 
+    const parsedBody = requiredBody.safeParse(req.body);
+
+    if (!parsedBody.success) {
+        res.json({
+            error: parsedBody.error,
+            msg: "Incorrect format",
+        })
+        return;
+    }
+
+    try {
+        const { username, password, lastName, firstName } = req.body;
+
+        const user = await userModel.create({
+            username,
+            password,
+            lastName,
+            firstName
+        })
+
+        if (user) {
+            res.json({
+                userId: user._id,
+                msg: "User signed "
+            })
+        }
+    } catch (error) {
+        res.json({
+            error: error,
+            msg: "Error occured while signing up"
+        })
+    }
+
+})
 
 
 export { userRouter };
