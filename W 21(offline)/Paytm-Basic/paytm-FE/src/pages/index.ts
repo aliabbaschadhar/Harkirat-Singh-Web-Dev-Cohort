a684d33ab68ea7a1cbad7df6a43ae6657e775faa
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-15 22:06:26 +0500

    Signin and Signup component done

--- W 21(offline)/Paytm-Basic/paytm-FE/src/pages/index.ts
+++ W 21(offline)/Paytm-Basic/paytm-FE/src/pages/index.ts
@@ -2,11 +2,21 @@ import Signin from "./Signin";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import SendMoney from "./SendMoney";
+import { Button } from "./Button";
+import { BottomWarning } from "./BottomWarning";
+import { Heading } from "./Heading";
+import { SubHeading } from "./SubHeading";
+import { InputBox } from "./InputBox";
 
 
 export {
     Signin,
     Signup,
     Dashboard,
-    SendMoney
+    SendMoney,
+    Button,
+    BottomWarning,
+    Heading,
+    SubHeading,
+    InputBox,
 }
