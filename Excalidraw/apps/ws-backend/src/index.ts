1a03a28d910dc18125c7d009171536f9cbc27b44
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-18 20:52:46 +0500

    Lets start ws layer

--- Excalidraw/apps/ws-backend/src/index.ts
+++ Excalidraw/apps/ws-backend/src/index.ts
@@ -4,6 +4,25 @@ import { JWT_SECRET } from "@repo/backend-common/config"
 
 const wss = new WebSocketServer({ port: 8081 });
 
+function checkUser(token: string): string | null {
+    if (!JWT_SECRET) {
+        console.log("JWT_SECRET is undefined")
+        return null;
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET)
+
+    if (typeof decoded === "string") {
+        return null;
+    }
+
+    if (!decoded || !(decoded as JwtPayload).userId) {
+        return null;
+    }
+
+    return (decoded as JwtPayload).userId;
+}
+
 // The websocket backend also needs to authorize the backend 
 // I will connect only if the user is authorized otherwise i will close the connection
 wss.on("connection", (ws, request) => {
@@ -15,18 +34,11 @@ wss.on("connection", (ws, request) => {
     const queryParams = new URLSearchParams(url.split("?")[1]); //["ws:localhost:8081", "token=1341114"]
     const token = queryParams.get('token') ?? "";
 
-    if (!JWT_SECRET) {
-        console.log("JWT_SECRET is undefined")
-        ws.close()
-        return;
-    }
-    const decoded = jwt.verify(token, JWT_SECRET)
+    const authorizedUser = checkUser(token);
 
-    if (!decoded || !(decoded as JwtPayload).userId) {
+    if (!authorizedUser) {
         ws.close();
-        return;
     }
-
 
     ws.on("message", (data) => {
         ws.send("pong");
